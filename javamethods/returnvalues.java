1cc058ad1382f7ca627c7d5218de4e070459f7b3
Yavuz <yvzyldz20@gmail.com>
2023-08-10 17:14:18 +0300

    Create returnvalues.java

---
+++ javamethods/returnvalues.java
@@ -0,0 +1,15 @@
+package javamethods;
+
+public class returnvalues {
+	static int myMethod(int x, int y) {
+		return x + y;
+	}
+	
+
+	public static void main(String[] args) {
+		System.out.println(myMethod(5,3));
+	   	
+
+	}
+
+}
