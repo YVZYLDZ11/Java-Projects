176dbc1c0112d84932c6bc06a0273ea4bfe6d2f0
Yavuz <yvzyldz20@gmail.com>
2023-08-10 17:13:40 +0300

    Create javamethods.java

---
+++ javamethods/javamethods.java
@@ -0,0 +1,15 @@
+package javamethods;
+
+public class javamethods {
+	static void myMethod(String fname, int age) {
+		System.out.println(fname + " Refsnes" + " is " + age);
+	}
+
+	public static void main(String[] args) {
+		myMethod("Liam", 5);
+		myMethod("Jenny", 8);
+		myMethod("Anja", 30);
+
+	}
+
+}
